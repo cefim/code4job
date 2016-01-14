e97a9a9dac582d581b40c39c62d9e8cf1452f8f0
fgauttron <fauttron@cefim.eu>
2016-01-14 23:38:50 +0100

    first commit

---
+++ sendemail.php
@@ -0,0 +1,18 @@
+<?php
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+$name       = @trim(stripslashes($_POST['name'])); 
+$from       = @trim(stripslashes($_POST['email'])); 
+$subject    = @trim(stripslashes($_POST['subject'])); 
+$message    = @trim(stripslashes($_POST['message'])); 
+$to   		= 'fgauttron@cefim.eu';//replace with your email
+
+$headers = 'MIME-Version: 1.0'."\n";
+$headers .= 'Content-type: text/plain; charset=iso-8859-1'."\n";
+$headers .= 'From:'. $name .'<'. $from .'>'."\n";
+$headers .= 'Reply-To:'. $from ."\n";
+$headers .= 'Subject: {$subject}'."\n";
+$headers .= 'X-Mailer: PHP/'.phpversion();
+
+mail($to, $subject, $message, $headers);
+
+die;
